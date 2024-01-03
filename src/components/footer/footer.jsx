9513c335b879223c1e5c42fc216e408bcbb15908
Furkan Coskun <furkdurk7@aol.com>
2024-01-03 18:38:47 -0500

    made a navbar + footer (nothing really changed)

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+      <footer className="footer">
+        <p>copyright@deezNuts inc. All rights reserved</p>
+      </footer>
+    );
+  };
+export default Footer
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+      <nav className="navBar">
+        <ul className="navList">
+          <li className="navListItem">Home</li>
+          <li className="navListItem">Help</li>
+          <li className="navListItem">Sign-In</li>
+        </ul>
+      </nav>
+    );
+  };
+export default Navbar
