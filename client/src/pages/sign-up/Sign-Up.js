2d6da2355058b64e8b082e3e124d0286873af4e1
Furkan Coskun <furkdurk7@aol.com>
2024-01-10 21:22:22 -0500

    created email validation + styling on signup form + error handling

--- client/src/pages/sign-up/Sign-Up.js
+++ client/src/pages/sign-up/Sign-Up.js
@@ -10,6 +10,8 @@ export const signUpDevLogs = false;
 export default function SignUp() {
   let { users, setUsers } = useContext(StateContext);
   let [formData, setFormData] = useState({});
+  let [signUpEmailError, setSignUpEmailError] = useState(``);
+
   const [addUserMutation, { loading, error, data }] = useMutation(gql`
     mutation AddUser($newUser: NewUserInput!) {
       addUser(newUser: $newUser) {
@@ -25,9 +27,38 @@ export default function SignUp() {
   if (data && signUpDevLogs == true) console.log(`Data`, data);
   
   const updateFormState = (e) => {
+    let { name, value } = e.target;
+
+    if (name == `email`) {
+      let validDomains = [`com`, `org`, `gov`, `edu`];
+      let userEmails = users.map(usr => usr.email.toLowerCase());
+      value = value.toLowerCase();
+
+      if (value.length >= 7) {
+        if (value.includes(`@`)) {
+          let emailWebsite = value.split(`@`)[1];
+          let emailDomain = emailWebsite.split(`.`)[1];
+          let emailHasValidDomain = validDomains.includes(emailDomain);
+          if (emailHasValidDomain == true) {
+            if (userEmails.includes(value)) {
+              setSignUpEmailError(`This email has already been registered`);
+            } else {
+              setSignUpEmailError(``);
+            }
+          } else {
+            setSignUpEmailError(`Must include part after @ symbol${emailHasValidDomain == false ? ` with a valid domain` : ``}`);
+          }
+        } else {
+          setSignUpEmailError(`Email must include an @ symbol`);
+        }
+      } else {
+        setSignUpEmailError(``);
+      }
+    }
+
     setFormData(prevFormData => ({
       ...prevFormData,
-      [e.target.name]: e.target.value,
+      [name]: value,
     }))
   }
 
@@ -36,19 +67,22 @@ export default function SignUp() {
   
     try {
       let { email, password } = formData;
-      const { data } = await addUserMutation({
-        variables: {
-          newUser: {
-            email,
-            password,
-            username: formData.email + `-${users.length + 1}`,
+      if (signUpEmailError == ``) {
+        const { data } = await addUserMutation({
+          variables: {
+            newUser: {
+              email,
+              password,
+              username: formData.email + `-${users.length + 1}`,
+            },
           },
-        },
-      });
-
-      if (data.addUser != null && data.addUser != undefined) {
-        console.log(`User added successfully`, data.addUser);
-        setUsers(prevUsers => [...prevUsers, data.addUser]);
+        });
+  
+        if (data.addUser != null && data.addUser != undefined) {
+          console.log(`User added successfully`, data.addUser);
+          setUsers(prevUsers => [...prevUsers, data.addUser]);
+          e.target.reset();
+        }
       }
     } catch (error) {
       console.log(`Error adding user`, error);
@@ -57,6 +91,14 @@ export default function SignUp() {
     }
   };
 
+  const isFormSubmitDisabled = () => {
+    let disabled = false;
+    if (signUpEmailError != ``) {
+      disabled = true;
+    }
+    return disabled;
+  }
+
   return (
     <>
       <Header />
@@ -64,10 +106,11 @@ export default function SignUp() {
         <Section title={`Sign Up`} />
         <section id={`signup`} className={`signupContentSection flex alignCenter justifyCenter flexColumn`} style={{padding: 15}}>
           <h2>Sign Up {users && Array.isArray(users) && <div className={`usersDiv`}>{users.length} User(s)</div>}</h2>
-          <form onChange={(e) => updateFormState(e)} onSubmit={(e) => onFormSubmit(e)} id={`signUpForm`} className={`flex flexColumn gap5 signUpForm`}>
+          <form onChange={(e) => updateFormState(e)} onSubmit={(e) => onFormSubmit(e)} id={`signUpForm`} className={`flex flexColumn gap5 signUpForm registrationForm`}>
             <input id={`email`} name={`email`} placeholder={`Enter Email...`} type={`email`} required />
+            {signUpEmailError != `` && <div className={`signUpEmailError errorMessage`}>{signUpEmailError}</div>}
             <input id={`password`} name={`password`} placeholder={`Enter Password...`} type={`password`} required />
-            <button type={`submit`}>Submit</button>
+            <button type={`submit`} className={`${isFormSubmitDisabled() == true ? `disabled` : ``}`} disabled={isFormSubmitDisabled()}>Submit</button>
           </form>
         </section>
       </main>
