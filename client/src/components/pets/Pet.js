b5e4ea4970bce7c5744d101ec7afd6d18ee72248
Alux <plutocoding@gmail.com>
2024-01-12 18:25:44 -0500

    repurposed adoption button for when user is signed out

--- client/src/components/pets/Pet.js
+++ client/src/components/pets/Pet.js
@@ -1,7 +1,21 @@
+import { useContext } from "react";
+import { StateContext } from "../../App";
+import { useNavigate } from "react-router-dom";
+
 export default function Pet({pet}) {
+    const navigate = useNavigate();
+    let { user } = useContext(StateContext);
 
     const adoptPet = () => {
         console.log(`Adopt Me`, pet);
+    }
+
+    const isAdoptionDisabled = () => {
+        let disabled = false;
+        if (user == null) {
+          disabled = true;
+        }
+        return disabled;
     }
 
     return (
@@ -11,7 +25,10 @@ export default function Pet({pet}) {
                 <div className={`petName petField`}>{pet.name}</div>
                 <div className={`petSpecies petField`}>{pet.species}, Age {pet.age && pet.age}</div>
             </div>
-            <button className={`adoptPetButton blackButton`} onClick={() => adoptPet()}>Adopt Me</button>
+            {/* Conditionally render the adopt button display dependin on if they are logged in or not. */}
+            {/* In addition to not being signed in, when the click the button, re-direct to the sign in page. */}
+            {/* Try to re-purpose something instead of deleting and wasting it.*/}
+            <button className={`adoptPetButton blackButton`} onClick={() => isAdoptionDisabled() == true ? navigate(`/sign-in`) : adoptPet()}>{isAdoptionDisabled() == true ? `Sign In to Adopt` : `Adopt Me`}</button>
         </div>
     )
 }
