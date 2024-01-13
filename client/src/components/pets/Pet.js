6d72604713b454c4d96e0432e721e400cf828c74
Alux <plutocoding@gmail.com>
2024-01-12 21:39:59 -0500

    simulated adoption process (but its not on the server yet) - messed with adoption button and conditional displays

--- client/src/components/pets/Pet.js
+++ client/src/components/pets/Pet.js
@@ -4,18 +4,51 @@ import { useNavigate } from "react-router-dom";
 
 export default function Pet({pet}) {
     const navigate = useNavigate();
-    let { user } = useContext(StateContext);
+    let { user, setPets } = useContext(StateContext);
 
     const adoptPet = () => {
+        // We are updating the pet here (based on the user pressing the enabled adopt button)
+        pet.adopted = true;
+        pet.ownerId = user._id;
+        pet.updatedAt = new Date().toLocaleString();
+        
+        // Once we update the pet object, we need to store that pet object back in the pets array.
+        // This is done in state, so it will automatically update the cards as they are changed.
+        setPets(prevPets => {
+            let updatedPetsAfterAdoption = prevPets.map(pt => {
+                if (pt._id == pet._id) {
+                    return pet;
+                } else {
+                    return pt;
+                }
+            })
+
+            return updatedPetsAfterAdoption;
+        });
+
         console.log(`Adopt Me`, pet);
     }
 
     const isAdoptionDisabled = () => {
         let disabled = false;
-        if (user == null) {
+        if (user == null || pet.adopted == true) {
           disabled = true;
         }
         return disabled;
+    }
+
+    const determineCardButtonText = () => {
+        let buttonText = `Adopt Me`;
+        
+        if (isAdoptionDisabled() == true) {
+            if (pet.adopted == true) {
+                buttonText = `Adopted`;
+            } else {
+                buttonText = `Sign In to Adopt`;
+            }
+        }
+
+        return buttonText;
     }
 
     return (
@@ -28,7 +61,7 @@ export default function Pet({pet}) {
             {/* Conditionally render the adopt button display dependin on if they are logged in or not. */}
             {/* In addition to not being signed in, when the click the button, re-direct to the sign in page. */}
             {/* Try to re-purpose something instead of deleting and wasting it.*/}
-            <button className={`adoptPetButton blackButton`} onClick={() => isAdoptionDisabled() == true ? navigate(`/sign-in`) : adoptPet()}>{isAdoptionDisabled() == true ? `Sign In to Adopt` : `Adopt Me`}</button>
+            <button disabled={isAdoptionDisabled()} className={`adoptPetButton blackButton ${isAdoptionDisabled() == true ? `disabled` : ``}`} onClick={() => isAdoptionDisabled() == true ? navigate(`/sign-in`) : adoptPet()}>{determineCardButtonText()}</button>
         </div>
     )
 }
